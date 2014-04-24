b579847967f41336e9a02864347a151c5abb91e9
alain <alain.castonguay.1@ulaval.ca>
2014-04-24 16:54:07 -0400

    first version

---
+++ README.md
@@ -0,0 +1,8 @@
+Projet Final
+
+Alain Castonguay
+Marie-Hélene Doré
+
+
+Cours : SIO 2109 - Introduction a la programmation
+
---
+++ educ_memory_game.dart
@@ -0,0 +1,32 @@
+library educ_memory_game;
+
+import 'dart:html';
+import 'dart:async';
+import 'dart:math';
+
+part 'model/cell.dart';
+part 'model/memory.dart';
+part 'view/board.dart';
+part 'util/images.dart';
+part 'util/random.dart';
+
+var niveau ='Débutant';
+
+
+
+void main() {
+  CanvasElement canvas = querySelector('#canvas');
+  ButtonElement play = querySelector('#play');
+
+  play.onClick.listen((Event e) {
+    window.location.reload();
+  });
+  
+ 
+ 
+          
+  new Board(canvas, new Memory(6));
+}
+
+
+
